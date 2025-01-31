77aabe750649099d3481585273e8f2c2fd11b797
amir hossein <amirhossein00978@gmail.com>
2025-01-31 15:27:19 +0330

    مقادیر مورد استفاده در سامانه پیامک اضافه شد

--- BlockChainSMS/Entities/Transaction.cs
+++ BlockChainSMS/Entities/Transaction.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace BlockChainSMS.Entities
 {
-    internal class Transaction
+    public class Transaction
     {
+        public Transaction(string sender, string receiver, string message, double fee)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            Message = message;
+            Fee = fee;
+            Timestamp = DateTime.Now;
+        }
+        public string Sender { get; }
+        public string Receiver { get; }
+        public string Message { get; }
+        public double Fee { get; }
+        public DateTime Timestamp { get; }
+
+        public override string ToString()
+        {
+            return $"{Sender} -> {Message} | Message : {Message} Fee : {Fee} Time : {Timestamp}";
+        }
     }
 }

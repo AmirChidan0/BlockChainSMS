6716b67f4107ed7700009981f7e19cd2b56c9f0b
amir hossein <amirhossein00978@gmail.com>
2025-01-31 15:25:04 +0330

    ورودی هش کردن در بلاک اضافه شد

--- BlockChainSMS/Entities/Block.cs
+++ BlockChainSMS/Entities/Block.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlockChainSMS.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace BlockChainSMS.Entities
 {
-    internal class Block
+    public class Block
     {
+        public Block(int index, string previousHash)
+        {
+            Index = index;
+            PreviousHash = previousHash;
+            Timestamp = DateTime.Now;
+            Transactions = new List<Transaction>();
+        }
+        public int Index { get; }
+        public string PreviousHash { get; }
+        public string Hash { get; private set; }
+        public DateTime Timestamp { get; }
+        public List<Transaction> Transactions { get; }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            Transactions.Add(transaction);
+        }
+
+        public void MineBlock(int difficulty)
+        {
+            string prefix = new string('0', difficulty);
+
+            do
+            {
+                Hash = Utility.GetSha256($"{Index}{PreviousHash}{Timestamp}{string.Join("", Transactions)}");
+            } while (!Hash.StartsWith(prefix));
+        }
+
+        public override string ToString()
+        {
+            return $"Block #{Index} [Hash : {Hash}, Transactions : {Transactions.Count}]";
+        }
     }
 }

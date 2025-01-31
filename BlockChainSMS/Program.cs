fdd5da3ee96de75e04d1c7011a370225ab71dc86
amir hossein <amirhossein00978@gmail.com>
2025-01-31 15:30:24 +0330

    فراخوانی و ثبت عملیات در Program اضافه شد

--- BlockChainSMS/Program.cs
+++ BlockChainSMS/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BlockChainSMS.Entities;
+
+var blockchain = new Contract(2, 10, 0.1);
+
+var Amir = new Account("1");
+var Hossein = new Account("2");
+
+blockchain.AddTransaction(new Transaction(Amir.Address, Hossein.Address, "Hello Amir!", 0.15));
+blockchain.MinePendingTransactions("Miner1");
+
+Console.WriteLine(blockchain);

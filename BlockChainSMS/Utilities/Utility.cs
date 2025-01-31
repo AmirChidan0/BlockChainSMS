5c9dc1ad9763d730e3080244785db623ce3c0bad
amir hossein <amirhossein00978@gmail.com>
2025-01-31 15:29:08 +0330

    sync

--- BlockChainSMS/Utilities/Utility.cs
+++ BlockChainSMS/Utilities/Utility.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlockChainSMS.Utilities
 {
-    internal class Utility
+    public class Utility
     {
+        public static string GetSha256(string input)
+        {
+            using var sha256 = SHA256.Create();
+            var bytes = Encoding.UTF8.GetBytes(input);
+            var hashBytes = sha256.ComputeHash(bytes);
+            return Convert.ToBase64String(hashBytes);
+        }
     }
 }
